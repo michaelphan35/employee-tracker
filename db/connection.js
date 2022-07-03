6719ab4e826f31f4ce7c128562d4aca660e3a68f
Michael Phan <michaelphan35@gmail.com>
2022-07-02 19:49:04 -0700

    added screenshots, removed pass

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Silver*Wind2319',
+    password: ' ',
     database: 'tracker'
 });
 
