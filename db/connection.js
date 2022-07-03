ed0cf40792ae012e6bc63ac4c5329fb75116a519
Michael Phan <michaelphan35@gmail.com>
2022-07-02 19:22:50 -0700

    fixed some seeding issues

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: ' ',
-    database: 'tracking'
+    password: 'Silver*Wind2319',
+    database: 'tracker'
 });
 
 module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,28 @@ USE tracker;
 
 CREATE TABLE department
 (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KET(id)
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE roles
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id) REFERENCES department(id);
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee
 (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NULL,
-    manager_id INT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id);
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id)
 );
